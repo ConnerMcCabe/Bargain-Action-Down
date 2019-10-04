a0c789913936d13848c73e1b5b0424241b504ecd
Admin <conner.p.mccabe@gmail.com>
2019-10-04 14:21:36 -0500

    set up and Auth

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
@@ -8,5 +10,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
